d3775167b8eb0d91364f690aff414bf6f0bfb04e
umarKhan1 <muhammadomar29@outlook.com>
2025-08-21 19:49:51 +0100

    user location fetch and get store in local done

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import Flutter
 import UIKit
-
+import GoogleMaps
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -8,6 +8,8 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+     GMSServices.provideAPIKey("AIzaSyCC2ZpWTa2HUPEADVMu8Lg_c1YvWUGLuNE")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
   }
 }
